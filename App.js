133563f03edfb0d782b8007d6f77d3bde1cd9141
Maksym Shokin <maksym.sh@upplabs.com>
2020-11-15 14:47:59 +0200

    android

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  const pushNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'First notifications',
+        body: 'Sent with expo'
+      },
+      trigger: {
+        seconds: 5
+      }
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='Push notification' onPress={pushNotificationHandler} />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -16,6 +29,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+//expo install expo-notifications
